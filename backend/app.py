91af688879ffe661f818a9cd9df41f8f1cfeece9
NerosJourney <sandman4264@gmail.com>
2024-04-20 20:20:42 -0400

    add report route

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,34 @@
 import sys 
 import os
 from scam_detection.text_analysis import analyze_spelling
+from scam_detection.job_analysis import Job
+from scam_detection.analyzer import Analyzer
+from db.db import *
 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/detect", methods=['POST'])
-def detect_route():
+analyzer = Analyzer()
+
+@app.route("/report", methods=['POST'])
+def report_route():
     request_data = request.get_json()
 
-    result = analyze_spelling(request_data["body"])
+    job = Job(request_data["domain"], request_data["link"])
 
-    response = {
-        "errors": result[0],
-        "total": result[1]
+    res = analyzer.run_analysis(job)
+
+    job_id = create_job(job)
+    create_report(res, job_id)
+
+    results = {
+        "job": job,
+        "report": res
     }
 
-    return jsonify(response)
+    return jsonify(results)
+
 
 @app.route("/report", methods=['POST'])
 def report():
