1d16c4503ddce3300c24e9f7678867bdc6925856
NerosJourney <sandman4264@gmail.com>
2024-04-21 10:08:49 -0400

    add report get route

--- backend/app.py
+++ backend/app.py
@@ -8,50 +8,72 @@ app = Flask(__name__)
 
 analyzer = Analyzer()
 
-@app.route("/report", methods=['POST'])
+@app.route("/report", methods=['POST', 'GET'])
 def report_route():
-    request_data = request.get_json()
+    if request.method == 'POST':
+        request_data = request.get_json()
 
-    contact = ContactInfo(phone_number=request_data["phone_number"], email=request_data["email"], name=request_data["name"])
+        contact = ContactInfo(phone_number=request_data["phone_number"], email=request_data["email"], name=request_data["name"])
 
-    job = Job(request_data["domain"], request_data["link"], request_data["position_title"], request_data["description"], request_data["company"], request_data["post_date"], contact_info=contact)
+        job = Job(request_data["domain"], request_data["link"], request_data["position_title"], request_data["description"], request_data["company"], request_data["post_date"], contact_info=contact)
 
-    job.display()
+        job.display()
 
-    res = analyzer.run_analysis(job)
+        res = analyzer.run_analysis(job)
 
-    job_id = create_job(job)
-    create_report(res, job_id)
+        job_id = create_job(job)
+        create_report(res, job_id)
 
-    results = {
-        "job": {
-            "job_id": job_id,
-            "domain": job.domain,
-            "link": job.link,
-            "position_title": job.position_title,
-            "description": job.description,
-            "company": job.company,
-            "contact": {
-                "phone_number": request_data["phone_number"],
-                "email": job.contact_info.email,
-                "name": job.contact_info.name
+        results = {
+            "job": {
+                "job_id": job_id,
+                "domain": job.domain,
+                "link": job.link,
+                "position_title": job.position_title,
+                "description": job.description,
+                "company": job.company,
+                "contact": {
+                    "phone_number": request_data["phone_number"],
+                    "email": job.contact_info.email,
+                    "name": job.contact_info.name
+                }
+            },
+            "result": {
+                "spelling_error_count": res.spelling_error_count,
+                "grammar_error_count": res.grammar_error_count,
+                "email_suspicious": res.email_suspicious,
+                "link_suspicious": res.link_suspicious,
+                "phone_suspicious": res.phone_suspicious,
+                "pay_suspicious": res.pay_suspicious,
+                "ai_analysis": res.ai_analysis,
+                "ai_summary": res.ai_summary,
+                "ai_skills": res.ai_skills,
+                "cumulative_risk": res.cumulative_risk
             }
-        },
-        "result": {
-            "spelling_error_count": res.spelling_error_count,
-            "grammar_error_count": res.grammar_error_count,
-            "email_suspicious": res.email_suspicious,
-            "link_suspicious": res.link_suspicious,
-            "phone_suspicious": res.phone_suspicious,
-            "pay_suspicious": res.pay_suspicious,
-            "ai_analysis": res.ai_analysis,
-            "ai_summary": res.ai_summary,
-            "ai_skills": res.ai_skills,
-            "cumulative_risk": res.cumulative_risk
         }
-    }
 
-    return jsonify(results)
+        return jsonify(results)
+    else:
+        id = request.args.get('report_id')
+
+        report = get_report_by_id(id)
+
+        results = {
+            "result": {
+                "spelling_error_count": report.spelling_error_count,
+                "grammar_error_count": report.grammar_error_count,
+                "email_suspicious": report.email_suspicious,
+                "link_suspicious": report.link_suspicious,
+                "phone_suspicious": report.phone_suspicious,
+                "pay_suspicious": report.pay_suspicious,
+                "ai_analysis": report.ai_analysis,
+                "ai_summary": report.ai_summary,
+                "ai_skills": report.ai_skills,
+                "cumulative_risk": report.cumulative_risk
+            }
+        }
+
+        return jsonify(results)
 
 
 @app.route("/bookmark", methods=['POST', 'GET'])
