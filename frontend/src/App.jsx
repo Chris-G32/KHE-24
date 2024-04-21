eeec64da0304443cc90aa98700e977c26e24a226
NerosJourney <sandman4264@gmail.com>
2024-04-20 22:03:12 -0400

    Add clerk auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
+import {Header} from './components/Header'
 
 import './App.css'
 
 function App() {
     return (
         <>
+            <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
             </Routes>
