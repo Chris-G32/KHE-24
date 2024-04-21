320adae6f497df0dac964b3ca21c32e0740c5319
Chris-G32 <cgruska@kent.edu>
2024-04-21 09:46:21 -0400

    Succeeds on linked in and indeed

--- frontend/extension/src/App.tsx
+++ frontend/extension/src/App.tsx
@@ -3,15 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { ftruncate } from 'fs';
+import { getElementError } from '@testing-library/react';
+
+function elementToPlainText(element: HTMLElement|Element|undefined|null): string {
+  let text = element?.textContent || '';
+  text = text.trim().replace(/\s+/g, ' '); // Remove extra whitespace
+  return text;
+}
+
+function removeHtmlTags(html: string): string {
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  return div.innerText || div.textContent || '';
+}
+
+function parseDescription(){
+  let url=window.location.href
+  let element;
+  if(url.includes("indeed")){
+    element=document.getElementById("jobDescriptionText")
+  }
+  else if(url.includes("linked")){
+    element=document.getElementById("job-details")
+    element=element?.getElementsByClassName("mt4").item(0)
+  }
+
+  // Select elements with class names containing the keyword
+  // const elements = document.querySelectorAll(`[class*="j${keyword}"]`);
+  return removeHtmlTags(elementToPlainText(element));
+}
+
 
 function App() {
+  const parseData=()=>{
+    console.log("Description:")
+    console.log(parseDescription())
+  };
   const onclick = async () => {
     let [tab] = await chrome.tabs.query({ active: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id! }, 
-      func: () => {
-        console.log("posos");
-      }
+      func:parseData
     }
 
     );
